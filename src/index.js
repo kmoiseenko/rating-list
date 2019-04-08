4901cb85f871694444b07073c221175294afefe8
Konstantin Moiseenko <write.to.k.moiseenko@gmail.com>
2019-04-08 15:00:39 +0300

    Update global files

--- src/index.js
+++ src/index.js
@@ -3,8 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import io from 'socket.io-client';
+import ClientContext from './socket/socketContext.js';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const serverUrl = process.env.NODE_ENV === 'production' ?  '/' : 'ws://localhost:3001';
+const client = io(serverUrl, { transports: ['websocket'] });
+
+ReactDOM.render(
+  <div className="full-height">
+    <ClientContext.Provider value={ client }>
+      <App />
+    </ClientContext.Provider>
+  </div>
+  , document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
